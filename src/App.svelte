567a647c0505632319ff5def6bdc25de762ffd98
JANNE-ASUS\Janne <jan.blomqvist@advox.se>
2019-11-14 08:33:46 +0100

    Example code with svelte-router and svelma - not working together

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<!-- App.svelte -->
+<script>
+	import { Router, Link, Route } from "svelte-routing";
+// import NavLink from "./components/NavLink.svelte";
+// import { Home, About, Blog, BlogPost } from "./routes";
+
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+import Home from "./routes/home.svelte";
+import About from "./routes/about.svelte";
+import Blog from "./routes/blog.svelte";	
+// import BlogPost from "./routes/blogbost.svelte";	
+
+  export let url = "";
+</script>
+
+<Router url="{url}">
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="about">About</Link>
+    <Link to="blog">Blog</Link>
+  </nav>
+	<hr>
+  <div>
+    <!-- <Route path="blog/:id" component="{BlogPost}" /> -->
+    <Route path="blog" component="{Blog}" />
+    <Route path="about" component="{About}" />
+    <Route path="/"><Home /></Route>
+  </div>
+</Router>
+
+<hr>
+
+<ol>
+  <li>Växla mellan <strong>Home</strong> och <strong>About</strong> - det funkar perfekt</li>
+  <li>Välj <strong>Blog</strong>, så kommer den sidan ligga kvar även om URL i browser ändras.</li>
+	<li>Det går  att göra refresh i browsern, så kommer rätt sida fram.</li>
+</ol>
+
+<hr>
+
+<p>https://github.com/c0bra/svelma</p>
+<p>https://c0bra.github.io/svelma/components/field</p>
---
+++ src/routes/Blog.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { Field, Input } from 'svelma'
+</script>
+
+<h1 class="title">BLOG</h1>
+<h3 class="subtitle">Om jag lägger in Field och Input nedan så funkar det inte att byta sida</h3>
+
+<div class="container">
+
+  <p>Denna sida innehåller Field och Input komponenter från Svelma</p>
+
+    <Field label="Name">
+      <Input value="Rich Harris" />
+    </Field>
+
+  <p>URL kommer att ändras, men innehållet på sidan uppdateras inte!</p>
+
+</div>
